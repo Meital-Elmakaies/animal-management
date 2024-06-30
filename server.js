7096c302014f43e1d93f9a2bcfa90abe9043a576
Meital <meital1888@gmail.com>
2024-06-30 21:52:19 +0300

    Remove db and fix code to compile

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+require("@babel/register")({
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+});
+
 const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
 let db;
